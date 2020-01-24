c7fd24033cb872d8e5060a61e40f10b3e4b71a40
Samsouk <samuels.meyer@gmail.com>
2020-01-24 21:34:59 +0100

    Finalisation de FileFilterCSharp v 1.0

---
+++ FileFiltrerCSharp/FileFiltrerCSharp/TargetClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+
+namespace FileFiltrerCSharp
+{
+    public class Target
+    {
+        private string folderName;
+        private List<string> types = new List<string>();
+
+        public Target(string folderName, List<string> types)
+        {
+            this.folderName = folderName;
+            this.types = types;
+        }
+
+        public string FolderName
+        {
+            get { return folderName; }
+            set { folderName = value; }
+        }
+
+        public List<string> Types
+        {
+            get { return types; }
+            set { types = value; }
+        }
+    }
+}
