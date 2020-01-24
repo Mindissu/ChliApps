04a0eb1877b069b12cc6d57d1fb81f15c286f2eb
Samsouk <samuels.meyer@gmail.com>
2020-01-24 21:34:59 +0100

    Finalisation de FileFilter v 1.0

---
+++ FileFiltrerCSharp/FileFiltrerCSharp/FileMover.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FileFiltrerCSharp
+{
+    public class FileMover
+    {
+        public void MoveFile(List<string> types, string sourcePath, string targetPath)
+        {
+            if (!Directory.Exists(targetPath))
+            {
+                Directory.CreateDirectory(targetPath);
+            }
+
+            foreach (string fileType in types)
+            {
+                string[] sourcefiles = Directory.GetFiles(sourcePath, "*." + fileType);
+
+                foreach (string sourcefile in sourcefiles)
+                {
+                    string fileName = Path.GetFileName(sourcefile);
+                    string destFile = Path.Combine(targetPath, fileName);
+                    Console.WriteLine("Déplacement de '" + fileName + "' dans '" + targetPath );
+
+                    File.Move(sourcefile, destFile);
+                }
+            }
+        }
+    }
+}
---
+++ FileFiltrerCSharp/FileFiltrerCSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace FileFiltrerCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Target> targetList = new List<Target>();
+            targetList.Add(new Target("EXEs", new List<string>() { "ps1", "bat", "exe", "jar", "msi" }));
+            targetList.Add(new Target("DOCs", new List<string>() { "txt", "pdf", "rtf", "odt", "txt", "docx", "doc", "html", "htm" }));
+            targetList.Add(new Target("MEDIAs", new List<string>() { "wav", "avi", "mkv", "mp3", "mp4" }));
+            targetList.Add(new Target("PICTUREs", new List<string>() { "png", "gif", "ico", "mpg", "mpe", "mpeg", "jpe", "jpg", "jpeg", "jfif" }));
+            targetList.Add(new Target("ARCHIVEs", new List<string>() { "xml", "pps", "xls", "xlsx" }));
+            targetList.Add(new Target("ZIPs", new List<string>() { "zip", "rar", "torrent" }));
+            Console.WriteLine("Ajout des folders.");
+
+            FileMover fileMover = new FileMover();
+            
+            foreach(Target target in targetList)
+            {
+                string sourcePath = @"C:\Users\Afryk\Downloads\";
+
+                if (!Directory.Exists(sourcePath + target.FolderName)){
+                    Directory.CreateDirectory(sourcePath + target.FolderName);
+                    Console.WriteLine("Création du folder '" + target.FolderName + "'.");
+                }
+                string targetPath = @"C:\Users\Afryk\Downloads\" + target.FolderName;
+
+                fileMover.MoveFile(target.Types, sourcePath, targetPath);
+            }
+        }
+    }
+}
